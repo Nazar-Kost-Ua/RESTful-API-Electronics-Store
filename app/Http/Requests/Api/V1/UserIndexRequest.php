dbda246fd9790b2d54d0aaa47f75aecb8cb9aadb
Nazar Kost <kost.nazar.ua@gmail.com>
2025-11-11 18:38:26 +0000

    Create UserIndexRequest and config 'data'.

---
+++ app/Http/Requests/Api/V1/UserIndexRequest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Requests\Api\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserIndexRequest extends FormRequest
+{
+    protected ?array $allowedIncludes;
+    protected ?array $allowedFields;
+
+    public function __construct()
+    {
+        $this->allowedIncludes = config('data.allowed_includes.users.index') ?? [];
+        $this->allowedFields = config('data.allowed_fields.users.index') ?? [];
+        parent::__construct();
+    }
+
+    public function checkIncludes($attribute, $value, $fail): void
+    {
+        $includes = explode(',', $value);
+        foreach ($includes as $include) {
+            if (!in_array($include, $this->allowedIncludes)) {
+                $fail("The selected include '{$include}' is invalid.");
+            }
+        }
+    }
+
+    public function checkFields($attribute, $value, $fail): void
+    {
+        $key = str_replace('fields.', '', $attribute);
+        $fields = array_map('trim', explode(',', $value));
+
+        if (array_key_exists($key, $this->allowedFields)) {
+            foreach ($fields as $field) {
+                if (!in_array($field, $this->allowedFields[$key])) {
+                    $fail("The selected field '{$field}' for '{$key}' is invalid.");
+                }
+            }
+        } else {
+            $fail("Key '{$key}' is not valid for fields.");
+        }
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'page' => ['nullable', 'integer', 'min:1'],
+            'per_page' => ['nullable', 'integer', 'min:1', 'multiple_of:5', 'max:100'],
+            'include' => ['nullable', 'string', function ($attribute, $value, $fail) {
+                $this->checkIncludes($attribute, $value, $fail);
+            }],
+            'fields' => ['nullable', 'array'],
+            'fields.*' => ['string', 'distinct', function ($attribute, $value, $fail) {
+                $this->checkFields($attribute, $value, $fail);
+            }],
+        ];
+    }
+}
---
+++ config/data.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'allowed_includes' => [
+        'users' => [
+            'index' => ['orders', 'cart', 'wishlist', 'reviews'],
+        ],
+    ],
+    'allowed_fields' => [
+        'users' => [
+            'index' => [
+                'users' => [
+                    'id', 'first_name', 'last_name', 'email',
+                    'phone', 'created_at', 'updated_at', 'deleted_at'
+                ],
+                'orders' => [
+                    'id', 'order_ref', 'delivery_type_id',
+                    'payment_type_id', 'subtotal', 'total',
+                    'currency', 'status', 'created_at', 'updated_at'
+                ],
+                'cart' => [
+                    'id', 'quantity',
+                    'price', 'currency',
+                    'created_at', 'updated_at'],
+                'reviews' => [
+                    'id', 'content', 'rating',
+                    'status', 'created_at', 'updated_at'
+                ],
+            ],
+        ],
+    ],
+];
